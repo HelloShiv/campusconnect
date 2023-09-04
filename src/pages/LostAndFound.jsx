a0319faad8535f1a27ebe3d0b8128b7372117a62
HelloShiv <swagatswaroopparida@gmail.com>
2023-09-04 21:39:21 +0530

    added drawer menu for adding items in lost and found

--- src/pages/LostAndFound.jsx
+++ src/pages/LostAndFound.jsx
@@ -1,9 +1,11 @@
 import Search from "../components/Search"
 import Card from "../components/Card";
+import LostPopUp from "../components/LostPopUp";
 
 function LostAndFound(){
     return(<>
      <Search placeholder="Seach for the lost item ...."/>
+     <LostPopUp />
         <Card />
         </>
     );
