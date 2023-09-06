bddbd1c280b879e8c44042f9527573732dbd36a0
HelloShiv <swagatswaroopparida@gmail.com>
2023-09-06 14:46:27 +0530

    solved the bug for toggle of isLoggedInState from true to false

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,61 +1,80 @@
 import { createContext ,useContext ,useState ,useEffect} from "react";
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
 import { getAuth, 
     createUserWithEmailAndPassword , 
     sendEmailVerification ,
     onAuthStateChanged,
     signOut,
     signInWithEmailAndPassword} 
-    from 'firebase/auth'
+    from 'firebase/auth';
 import firebaseConfig from "./Api";
 import Password from "antd/es/input/Password";
+import { Space, Spin } from 'antd';
 
 
-const FirebaseContext = createContext(null);
+export const FirebaseContext = createContext(null);
 
 
 export const useFirebase = () => useContext(FirebaseContext);
 
-const firebaseApp = initializeApp(firebaseConfig);
+export  const {firebaseApp} = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
 
 export const FirebaseProvider = (props) => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [user ,setUser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(firebaseAuth, (user) => {
+      setUser(user);
+      setIsLoading(false); // Set isLoading to false once the user state is determined.
+    });
 
+    return () => {
+      // Cleanup the listener when the component unmounts.
+      unsubscribe();
+    };
+  }, []);
 
-    useEffect(() =>{
-        onAuthStateChanged(firebaseAuth ,user =>{
-           
-            if(user) setUser(user)
-            else setUser(null);
-        })
-    })
+  const SignupUserWithEmailAndPass = async (email, password) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        firebaseAuth,
+        email,
+        password
+      );
+      await sendEmailVerification(userCredential.user);
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+  };
 
-    const SignupUserWithEmailAndPass = async (email, password) => { // Changed parameter names
-        try {
-          const userCredential = await createUserWithEmailAndPassword(firebaseAuth, email, password);
-          // Send email verification
-          await sendEmailVerification(userCredential.user); // Changed this line
-        } catch (error) {
-          // Handle any errors here
-          console.error("Error creating user:", error);
-        }
-      };
+  const signInUserWithEmailAndPass = (email, password) =>
+    signInWithEmailAndPassword(firebaseAuth, email, password);
 
-      const signInUserWithEmailAndPass = (email , password) => signInWithEmailAndPassword(firebaseAuth,email,password);
+  const isLoggedIn = !!user; // Use double negation to convert to a boolean.
 
-      const isLoggedIn = user ? true : false;
+  const SignOut = () => {
+    return signOut(firebaseAuth);
+  };
 
-      const SignOut = () => {
-        return signOut(firebaseAuth);
-      };
+  if (isLoading) {
+    return <div style={{display:"flex" ,justifyContent:"center" ,alignItems:"center" ,innerWidth:"100vw",height:"100vh"}}> 
+            
+      <Spin size="large" />
+    </div>
+  }
 
-    return <FirebaseContext.Provider 
-    value={{SignupUserWithEmailAndPass ,
-            signInUserWithEmailAndPass,
-            SignOut,
-            isLoggedIn}}>
-        {props.children}
-        </FirebaseContext.Provider>
+  return (
+    <FirebaseContext.Provider
+      value={{
+        SignupUserWithEmailAndPass,
+        signInUserWithEmailAndPass,
+        SignOut,
+        isLoggedIn,
+      }}
+    >
+      {props.children}
+    </FirebaseContext.Provider>
+  );
 };
