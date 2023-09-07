f771777cab2bb53c0e2e8298f9785b62e3ac85ba
HelloShiv <swagatswaroopparida@gmail.com>
2023-09-07 23:47:05 +0530

    added environment variables

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -7,7 +7,6 @@ import { getAuth,
     signOut,
     signInWithEmailAndPassword} 
     from 'firebase/auth';
-import firebaseConfig from "./Api";
 import Password from "antd/es/input/Password";
 import { Space, Spin } from 'antd';
 
@@ -16,6 +15,19 @@ export const FirebaseContext = createContext(null);
 
 
 export const useFirebase = () => useContext(FirebaseContext);
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+
 
 export  const {firebaseApp} = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
