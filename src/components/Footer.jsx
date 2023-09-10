18707373ff8b01de7f2cadfec9bd31155b83187b
HelloShiv <swagatswaroopparida@gmail.com>
2023-09-10 23:51:48 +0530

    added links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,21 +6,13 @@ return ( <footer>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" /  >
 <div className="footer">
 <div className="row">
-<a href="#"><i className="fa fa-facebook"></i></a>
-<a href="#"><i className="fa fa-instagram"></i></a>
-<a href="#"><i className="fa fa-youtube"></i></a>
-<a href="#"><i className="fa fa-twitter"></i></a>
+<a href="https://www.facebook.com/the.campus.connect/"><i className="fa fa-facebook"></i></a>
+<a href="https://www.facebook.com/the.campus.connect/"><i className="fa fa-instagram"></i></a>
+<a href="https://www.youtube.com/@CampusConnect-ok4hb"><i className="fa fa-youtube"></i></a>
+<a href="https://twitter.com/CampuConnect"><i className="fa fa-twitter"></i></a>
 </div>
 
-<div className="row">
-<ul>
-<li><a href="#">Contact us</a></li>
-<li><a href="#">Our Services</a></li>
-<li><a href="#">Privacy Policy</a></li>
-<li><a href="#">Terms & Conditions</a></li>
 
-</ul>
-</div>
 
 <div className="row">
 CampusConnect Copyright © 2023 
