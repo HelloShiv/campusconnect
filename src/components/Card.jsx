aa267174d2c7f153f9e23fb1e57792e8f0c7988a
HelloShiv <swagatswaroopparida@gmail.com>
2023-09-14 00:33:53 +0530

    added pagination in marketplace and lostandfound

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from "react";
 import CardItems from "./CardItems";
 import { useFirebase } from "../context/Firebase";
 import "../styles/card.css";
+import { Pagination } from "antd"; // Import Pagination from Ant Design
 
 function Card() {
   const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // Current page
+  const pageSize = 9; // Number of items per page
   const [isLoading, setIsLoading] = useState(true);
   const firebase = useFirebase();
 
   useEffect(() => {
-    firebase.listAllLostAndFoundItems()
+    firebase
+      .listAllLostAndFoundItems()
       .then((querySnapshot) => {
         const itemData = querySnapshot.docs.map((doc) => doc.data());
         setItems(itemData);
@@ -21,16 +25,36 @@ function Card() {
       });
   }, []);
 
+  // Function to handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  // Calculate the start and end index based on currentPage and pageSize
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
   return (
     <div className="main">
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <ul className="cards">
-          {items.map((item, index) => (
-            <CardItems key={index} {...item} />
-          ))}
-        </ul>
+        <div>
+          <ul className="cards">
+            {items.slice(startIndex, endIndex).map((item, index) => (
+              <CardItems key={index} {...item} />
+            ))}
+          </ul>
+          <Pagination
+            current={currentPage}
+            pageSize={pageSize}
+            total={items.length}
+            onChange={handlePageChange}
+            showSizeChanger={false} 
+            showQuickJumper={true} 
+            style={{display:"flex", justifyContent:"center" ,paddingBottom:"2vh"}}
+          />
+        </div>
       )}
     </div>
   );
--- src/components/MarketPlaceCard.jsx
+++ src/components/MarketPlaceCard.jsx
@@ -2,38 +2,56 @@ import React, { useEffect, useState } from "react";
 import { useFirebase } from "../context/Firebase";
 import "../styles/marketplace.css";
 import MarketplaceItems from "./MarketPlaceItems";
+import { Pagination } from "antd"; // Import Pagination from Ant Design
 
-function MarketplaceCard(props){
- 
-     const [items, setItems] = useState([]);
-     const [isLoading, setIsLoading] = useState(true);
-     const firebase = useFirebase();
-   
-     useEffect(() => {
-       firebase.listAllMarketplaceItems()
-         .then((querySnapshot) => {
-           const itemData = querySnapshot.docs.map((doc) => doc.data());
-           setItems(itemData);
-           setIsLoading(false);
-         })
-         .catch((error) => {
-           console.error("Error fetching data from Firebase:", error);
-           setIsLoading(false);
-         });
-     }, []);
-   
-     return (
-       <div className="main">
-         
-           <ul className="cards">
-             {items.map((item, index) => (
-               <MarketplaceItems key={index} {...item} />
-             ))}
-           </ul>
-         
-       </div>
-     );
+function MarketplaceCard(props) {
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // Current page
+  const pageSize = 9; // Number of items per page
+  const [isLoading, setIsLoading] = useState(true);
+  const firebase = useFirebase();
 
+  useEffect(() => {
+    firebase
+      .listAllMarketplaceItems()
+      .then((querySnapshot) => {
+        const itemData = querySnapshot.docs.map((doc) => doc.data());
+        setItems(itemData);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data from Firebase:", error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  // Function to handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  // Calculate the start and end index based on currentPage and pageSize
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  return (
+    <div className="main">
+      <ul className="cards">
+        {items.slice(startIndex, endIndex).map((item, index) => (
+          <MarketplaceItems key={index} {...item} />
+        ))}
+      </ul>
+      <Pagination
+        current={currentPage}
+        pageSize={pageSize}
+        total={items.length}
+        onChange={handlePageChange}
+        showSizeChanger={false} 
+        showQuickJumper={true} 
+        style={{marginBottom:"2vh" , display:"flex" ,justifyContent:"center"}}
+      />
+    </div>
+  );
 }
 
 export default MarketplaceCard;
